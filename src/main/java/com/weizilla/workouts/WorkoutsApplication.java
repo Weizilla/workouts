3da80aa0fa0788d20587db1352e2c739d5139b5f
Wei Yang <wei@weizilla.com>
2017-04-10 20:08:59 -0500

    Add Spring Boot skeleton

---
+++ src/main/java/com/weizilla/workouts/WorkoutsApplication.java
@@ -0,0 +1,12 @@
+package com.weizilla.workouts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkoutsApplication.class, args);
+    }
+}
